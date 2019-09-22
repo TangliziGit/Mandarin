29b01ef4b63c87f8164c8da66a8cf3900d4e85fa
TangliziGit <tanglizimail@foxmail.com>
2019-09-22 19:15:05 +0800

    api: search librarian function completed

--- mandarin-back/src/main/java/org/a3/mandarin/back/controller/SearchController.java
+++ mandarin-back/src/main/java/org/a3/mandarin/back/controller/SearchController.java
@@ -1,13 +1,12 @@
 package org.a3.mandarin.back.controller;
 
-import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import org.a3.mandarin.back.exception.ApiForbiddenException;
 import org.a3.mandarin.back.model.RESTfulResponse;
 import org.a3.mandarin.common.annotation.Permission;
 import org.a3.mandarin.common.dao.repository.UserQueryRepository;
-import org.a3.mandarin.common.dao.repository.UserRepository;
 import org.a3.mandarin.common.entity.QUser;
+import org.a3.mandarin.common.entity.Role;
 import org.a3.mandarin.common.entity.User;
 import org.a3.mandarin.common.enums.PermissionType;
 import org.a3.mandarin.common.util.RoleUtil;
@@ -46,9 +45,45 @@ public class SearchController {
         if (null == userId && null == name && null == phoneNumber && null == email)
             throw new ApiForbiddenException("please enter any parameters");
 
-        QUser qUser=QUser.user;
         PageRequest pageRequest=PageRequest.of(page, limit, Sort.by("signUpTime"));
-        BooleanExpression expression=qUser.roles.contains(RoleUtil.readerRole);
+
+        List<User> readers=findUserByInformationWithPageRequest(RoleUtil.readerRole, userId, name, phoneNumber, email, pageRequest);
+        RESTfulResponse<List<User>> response=RESTfulResponse.ok();
+        response.setData(readers);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/search/librarian")
+    @ResponseBody
+    @Transactional
+    @Permission(PermissionType.ADMIN)
+    public ResponseEntity<RESTfulResponse<List<User>>> searchLibrarian(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "limit", defaultValue = "20") Integer limit,
+            @RequestParam(value = "userId", required = false) Integer userId,
+            @RequestParam(value = "name", required = false) String name,
+            @RequestParam(value = "phoneNumber", required = false) String phoneNumber,
+            @RequestParam(value = "email", required = false) String email){
+        if (null == userId && null == name && null == phoneNumber && null == email)
+            throw new ApiForbiddenException("please enter any parameters");
+
+        PageRequest pageRequest=PageRequest.of(page, limit, Sort.by("signUpTime"));
+
+        List<User> librarians=findUserByInformationWithPageRequest(RoleUtil.librarianRole, userId, name, phoneNumber, email, pageRequest);
+        RESTfulResponse<List<User>> response=RESTfulResponse.ok();
+        response.setData(librarians);
+        return ResponseEntity.ok(response);
+    }
+
+    private List<User> findUserByInformationWithPageRequest(
+            Role role,
+            Integer userId,
+            String name,
+            String phoneNumber,
+            String email,
+            PageRequest pageRequest){
+        QUser qUser=QUser.user;
+        BooleanExpression expression=qUser.roles.contains(role);
 
         if (null != name) expression=expression.and(qUser.name.contains(name));
         if (null != email) expression=expression.and(qUser.email.eq(email));
@@ -56,9 +91,6 @@ public class SearchController {
         if (null != phoneNumber) expression=expression.and(qUser.phoneNumber.eq(phoneNumber));
 
         Page<User> userPage=userQueryRepository.findAll(expression, pageRequest);
-
-        RESTfulResponse<List<User>> response=RESTfulResponse.ok();
-        response.setData(userPage.getContent());
-        return ResponseEntity.ok(response);
+        return userPage.getContent();
     }
 }
