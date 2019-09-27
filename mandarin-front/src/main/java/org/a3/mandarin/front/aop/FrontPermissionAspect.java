25c8219788bd6526dbede35651b7a16bd9ad82c8
TangliziGit <tanglizimail@foxmail.com>
2019-09-27 21:22:21 +0800

    front: permission aop done

---
+++ mandarin-front/src/main/java/org/a3/mandarin/front/aop/FrontPermissionAspect.java
@@ -0,0 +1,67 @@
+package org.a3.mandarin.front.aop;
+
+import org.a3.mandarin.common.annotation.Permission;
+import org.a3.mandarin.common.aop.AbstractPermissionAspect;
+import org.a3.mandarin.common.dao.repository.UserRepository;
+import org.a3.mandarin.common.entity.User;
+import org.a3.mandarin.common.enums.PermissionType;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Arrays;
+import java.util.List;
+
+@Aspect
+@Component
+public class FrontPermissionAspect extends AbstractPermissionAspect {
+    private final Logger logger= LoggerFactory.getLogger(FrontPermissionAspect.class);
+
+    @Resource
+    private UserRepository userRepository;
+
+    @Pointcut("execution(public * org.a3.mandarin.front.controller.AdminManagementController.*(..))")
+    public void frontAdminPointcut(){}
+
+    @Around(value = "authorize(permission) && frontAdminPointcut()", argNames = "joinPoint,permission")
+    public Object adminDoAround(ProceedingJoinPoint joinPoint, Permission permission) throws Throwable{
+        ServletRequestAttributes attributes=(ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (null == attributes)
+            return "admin/404";
+
+        HttpServletRequest request=attributes.getRequest();
+        List<PermissionType> permissionTypes= Arrays.asList(permission.value());
+
+        if (null == request)
+            return "admin/404";
+
+        logger.info("CHECK PERMISSION: "+request.getRequestURL().toString());
+        HttpSession session=request.getSession();
+        if (null == session.getAttribute("userId"))
+            return "admin/403";
+
+        Integer userId=(Integer)session.getAttribute("userId");
+        User user=userRepository.findById(userId).orElse(null);
+
+        if (null == user)
+            return "admin/404";
+
+        logger.info("ROLE TYPE: "+ user.getRoles().toString());
+
+        if (!checkPermission(permissionTypes, user))
+            return "admin/403";
+
+        logger.info("PERMISSION PASSED: "+request.getRequestURL().toString());
+        return joinPoint.proceed();
+    }
+
+}
--- mandarin-front/src/main/java/org/a3/mandarin/front/controller/AdminManagementController.java
+++ mandarin-front/src/main/java/org/a3/mandarin/front/controller/AdminManagementController.java
@@ -1,5 +1,7 @@
 package org.a3.mandarin.front.controller;
 
+import org.a3.mandarin.common.annotation.Permission;
+import org.a3.mandarin.common.enums.PermissionType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,28 +15,32 @@ public class AdminManagementController {
         return "admin/login";
     }
 
-    // TODO: permission
     @GetMapping({"/index", "/welcome"})
+    @Permission(PermissionType.ADMIN)
     public String index(){
         return "admin/index";
     }
 
     @GetMapping({"/search"})
+    @Permission(PermissionType.ADMIN)
     public String search(){
         return "admin/search";
     }
 
     @GetMapping({"/register"})
+    @Permission(PermissionType.ADMIN)
     public String registerLibrarian(){
         return "admin/register";
     }
 
     @GetMapping({"/setting"})
+    @Permission(PermissionType.ADMIN)
     public String setting(){
         return "admin/setting";
     }
 
     @GetMapping({"/about"})
+    @Permission(PermissionType.ADMIN)
     public String about(){
         return "admin/about";
     }
