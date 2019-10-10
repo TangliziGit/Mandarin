675f4517ed336ac6b93d9cc1140d006c541f3863
TangliziGit <tanglizimail@foxmail.com>
2019-10-10 23:51:46 +0800

    common: validate some fields

--- mandarin-common/src/main/java/org/a3/mandarin/common/util/ValidateUtil.java
+++ mandarin-common/src/main/java/org/a3/mandarin/common/util/ValidateUtil.java
@@ -1,23 +1,32 @@
 package org.a3.mandarin.common.util;
 
+import org.apache.commons.validator.routines.EmailValidator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ValidateUtil {
+    private static Pattern phoneNumberPattern = Pattern.compile("^[1][3,4,5,7,8][0-9]{9}$");
+    private static EmailValidator emailValidator=EmailValidator.getInstance();
+
     public static Boolean validatePassword(String password){
-        // TODO
+        if (password.length()<2)
+            return false;
         return true;
     }
 
     public static Boolean validatePhoneNumber(String phoneNumber){
-        // TODO
-        return true;
+        Matcher m = phoneNumberPattern.matcher(phoneNumber);
+        return m.matches();
     }
 
     public static Boolean validateEmail(String email){
-        // TODO
-        return true;
+        return emailValidator.isValid(email);
     }
 
     public static Boolean validateName(String name){
-        // TODO
+        if (name.length()<2)
+            return false;
         return true;
     }
     
